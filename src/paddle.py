75fdfdd012f3b782c64fb2435dbefbd657ff90a7
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-08-23 02:33:24 +0300

    Update paddle.py

--- src/paddle.py
+++ src/paddle.py
@@ -9,12 +9,14 @@ class Paddle(Turtle):
         self.goto(x=350, y=0)
 
     def moveUp(self):
-        Y = self.ycor()
-        self.sety(y=Y + 40)
+        if self.ycor() < 240:       # To make sure the paddle does not moves any forward after reaching the maximum boundary of the screen
+            Y = self.ycor()
+            self.sety(y=Y + 40)
 
     def moveDown(self):
-        Y = self.ycor()
-        self.sety(y=Y - 40)
+        if self.ycor() > -240:      # To make sure the paddle does not moves any forward after reaching the maximum boundary of the screen
+            Y = self.ycor()
+            self.sety(y=Y - 40)
 
     def move_to_other_side(self):
         self.goto(x=-350, y=0)
